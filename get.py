d49f6362c5322bf16229bd4df57cf2dd6459714e
YIFEI CAO <yifeicao@YIFEInoMacBook-Air.local>
2026-05-13 16:16:32 -0700

    add: test API

---
+++ get.py
@@ -0,0 +1,31 @@
+import requests
+import json
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# Specify the search term
+query = '"computer science"'
+
+# Define the API endpoint URL
+url = "http://api.semanticscholar.org/graph/v1/paper/search/bulk"
+
+# Define the query parameters
+query_params = {
+    "query": '"generative ai"',
+    "fields": "title,url,publicationTypes,publicationDate,openAccessPdf",
+    "year": "2023-"
+}
+
+# Directly define the API key (Reminder: Securely handle API keys in production environments)
+api_key = os.getenv("API_KEY") # Replace with the actual API key
+
+# Define headers with API key
+headers = {"x-api-key": api_key}
+
+# Send the API request
+response = requests.get(url, params=query_params, headers=headers).json()
+
+with open("results.json", "w") as f:
+    json.dump(response, f, indent=2)
